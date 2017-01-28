277710668431d10621a5e38e1033b15df04a36d3
Brendan Bruce <brendanbruce@gmail.com>
2017-01-28 10:47:23 -0500

    Conversion to hsl

--- script2.js
+++ script2.js
@@ -15,21 +15,60 @@ chrome.runtime.onMessage.addListener(
 
       var uniqueColors = [];
 
-      $.each(colors, function(i, el){
-        if($.inArray(el, uniqueColors) === -1) uniqueColors.push(el);
+      $.each(colors, function(index, value){
+        var lowerCaseColor = value.toLowerCase();
+
+        if (lowerCaseColor.length <7) {
+          switch (lowerCaseColor.length) {
+            case 4:
+              lowerCaseColor = lowerCaseColor + lowerCaseColor.slice(1, 4);
+              break;
+            case 2:
+              lowerCaseColor = lowerCaseColor + (lowerCaseColor.slice(1, 2) * 2);
+              break;
+          }
+        }
+
+        if($.inArray(lowerCaseColor, uniqueColors) === -1) {
+          uniqueColors.push(lowerCaseColor);
+        }
       });
 
+      var colorSet = [];
 
-      $.each(uniqueColors, function(index,value) {
+      $.each(uniqueColors, function(index, value) {
+        var red = parseInt("0x" + value.slice(1, 2));
+        var blue = parseInt("0x" + value.slice(3, 4));
+        var green = parseInt("0x" + value.slice(5, 6));
+
+        var hsl = rgbToHsl(red, blue, green);
+
+        var colorObject = {
+          hex: value,
+          hue: hsl[0],
+          saturation: hsl[1],
+          lightness: hsl[2]
+        };
+
+        colorSet.push(colorObject);
+      });
+
+      $.each(colorSet, function(index,value) {
+        var colorSwatchText = 
+          $("<div/>")
+          .attr("class", "swatch__text")
+          .html("<h5>" + value.hex + "</h5>");
+
         var colorSwatchInner =
           $("<div/>")
           .attr("class", "swatch__inner")
-          .css("backgroundColor", value);
+          .css("backgroundColor", value.hex);
 
         var colorSwatch =
           $("<div/>")
           .attr("class", "swatch")
-          .html(colorSwatchInner);
+          .html(colorSwatchInner)
+          .append(colorSwatchText);
 
         var swatchDOM = $("#swatchContainer").append($(colorSwatch));
       });
@@ -65,6 +104,30 @@ chrome.runtime.onMessage.addListener(
         });
 
         return stylesheets;
+      }
+
+      function rgbToHsl(r, g, b) {
+        r /= 255, g /= 255, b /= 255;
+
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+
+        if (max == min) {
+          h = s = 0; // achromatic
+        } else {
+          var d = max - min;
+          s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+          switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+          }
+
+          h /= 6;
+        }
+
+        return [ h, s, l ];
       }
     }
   }
